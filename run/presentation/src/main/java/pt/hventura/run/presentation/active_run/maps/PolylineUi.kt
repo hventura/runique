983a976bb086098a8f6b8584ec88a2533fd8b98b
Helder V. <hfs.ventura@gmail.com>
2024-11-03 23:53:01 +0000

    Drawing poly lines with colors in map on tracking run;

---
+++ run/presentation/src/main/java/pt/hventura/run/presentation/active_run/maps/PolylineUi.kt
@@ -0,0 +1,10 @@
+package pt.hventura.run.presentation.active_run.maps
+
+import androidx.compose.ui.graphics.Color
+import pt.hventura.core.domain.location.Location
+
+data class PolylineUi(
+    val location1: Location,
+    val location2: Location,
+    val color: Color
+)
